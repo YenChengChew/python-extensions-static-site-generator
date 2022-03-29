2f5d9681bd72dd2efb4bff80518d5ef0ad46ccba
Yen Cheng Chew <yenchengchew@gmail.com>
2022-03-30 09:00:24 +1030

    update

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -3,14 +3,14 @@ import importlib
 from pathlib import Path
 
 def load_module(directory, name):
-    sys.path.insert(directory[0])
+    sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
 
 def load_directory(directory):
-    for path in directory.Path.rglob(".py"):
+    for path in directory.rglob(".py"):
         load_module(directory.as_posix(), path.stem)
 
 def load_bundled():
-    directory = Path(__file__).parent / extionsions
+    directory = Path(__file__).parent / "extionsions"
     load_directory(directory)
